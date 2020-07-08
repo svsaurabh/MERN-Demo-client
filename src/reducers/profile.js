60d35a60b2ab7af523773317a0e0b3b13a8ceadf
svsaurabh <svsaurabh3232@gmail.com>
2020-07-09 02:13:44 +0530

    Updated till profile

--- src/reducers/profile.js
+++ src/reducers/profile.js
@@ -1,8 +1,10 @@
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_ERROR,
   CLEAR_PROFILE,
   UPDATE_PROFILE,
+  GET_REPOS
 } from "../actions/types";
 
 const initialState = {
@@ -24,19 +26,30 @@ export default function (state = initialState, action) {
         profile: payload,
         loading: false,
       };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false
+      };
     case PROFILE_ERROR:
       return {
         ...state,
         error: payload,
-        loading: false,
+        loading: false
       };
     case CLEAR_PROFILE:
       return {
         ...state,
         profile: null,
         repos: [],
-        loading: false,
       };
+    case GET_REPOS:
+      return{
+        ...state,
+        repos:payload,
+        loading:false
+      }
     default:
       return state;
   }
